43b550c52f3c63ffc6f28e4a364389b7f60d4857
rahul-pandey-20330 <rahulpandeyr184@gmail.com>
2025-08-24 21:04:55 +0530

    2nd question file's are added

---
+++ Day 1 Question2.js
@@ -0,0 +1,21 @@
+//Write A Program to Calculate Area Of Reactangle And Parameter Of Reactangle
+const { log } = require("console");
+let readline = require("readline");
+let rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout 
+});
+rl.question("Enter Length Of Reactangle : ", (len)=>{
+    rl.question("Enter Width Of Reactangle : ", (wid)=>{
+        // convert into number
+        let length = Number(len);
+        let width = Number(wid);
+
+        let area = length * width ;
+        let Parameter =2* (length + width);
+        console.log("Area Of Reactangle is ", area ,
+            "\n Parameter of Reactangle is :", Parameter);
+            rl.close();
+    });
+});
+    
